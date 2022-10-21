eef8d454265c627181d971ffa0d95a39f4eff8e9
Pablo Gonzalez <gonzalezp0813@gmail.com>
2022-10-20 21:59:24 -0500

    add snake movement

--- src/App.js
+++ src/App.js
@@ -19,10 +19,10 @@ function createBoard(size) {
 }
 
 const DIRECTIONS = {
-	LEFT: 'LEFT',
-	RIGHT: 'RIGHT',
-	UP: 'UP',
-	DOWN: 'DOWN',
+	LEFT: 'ArrowLeft',
+	RIGHT: 'ArrowRight',
+	UP: 'ArrowUp',
+	DOWN: 'ArrowDown',
 }
 
 const INITIAL_CELL = [1, 1]
@@ -34,11 +34,35 @@ const INITIAL_SNAKE = {
 }
 
 function moveSnake(snake) {
+	if (snake === null) throw 'snake is required, actual type: ' + typeof snake
 
+	const lastSnakeValue = snake.position[snake.position.length - 1]
+	let newHeadPositon
+
+	if (snake.direction === DIRECTIONS.UP)
+		newHeadPositon = [snake.head[0] - 1, snake.head[1]]
+	else if (snake.direction === DIRECTIONS.RIGHT)
+		newHeadPositon = [snake.head[0], snake.head[1] + 1]
+	else if (snake.direction === DIRECTIONS.DOWN)
+		newHeadPositon = [snake.head[0] + 1, snake.head[1]]
+	else if (snake.direction === DIRECTIONS.LEFT)
+		newHeadPositon = [snake.head[0], snake.head[1] - 1]
+
+	let newPosition = snake.position.filter(
+		(coords) => coords[0] !== lastSnakeValue[0] && coords !== lastSnakeValue[1]
+	)
+	newPosition = [newHeadPositon, ...newPosition]
+
+	let newSnake = {
+		...snake,
+		head: newHeadPositon,
+		position: newPosition,
+	}
+	return newSnake
 }
 
 // if position contains x and y return true
-function validateSquare(snake, x, y) { 
+function validateSquare(snake, x, y) {
 	const validation =
 		snake.position.filter((coords) => coords[0] === x && coords[1] === y)
 			.length >= 1
@@ -48,32 +72,59 @@ function validateSquare(snake, x, y) {
 function Board() {
 	const [snake, setSnake] = React.useState(INITIAL_SNAKE)
 
+  console.log(snake)
+
+	React.useEffect(() => {
+		document.addEventListener('keydown', (event) => {
+			const keyCode = event.code
+
+			if (
+				!(
+					keyCode === DIRECTIONS.UP ||
+					keyCode === DIRECTIONS.RIGHT ||
+					keyCode === DIRECTIONS.DOWN ||
+					keyCode === DIRECTIONS.LEFT
+				)
+			)
+				return
+
+			setSnake(last => ({ ...last, direction: keyCode }))
+		})
+	}, [])
+
 	const mBoard = createBoard(DEFAULT_DIMENSIONS)
 
-	console.log(mBoard)
+	const manualMove = () => {
+		setSnake(moveSnake(snake))
+	}
 
 	return (
-		<div className="board_container">
-			{mBoard.map((rowInfo, rowIndex) => {
-				return (
-					<div className="row" key={Math.random()}>
-						{rowInfo.map((x, squareIndex) => {
-							const isSnake = validateSquare(
-								snake,
-								Number(rowIndex),
-								Number(squareIndex)
-							)
-							return (
-								<div
-									className={`square ${isSnake ? 'is-snake' : ''}`}
-									key={Math.random()}
-								></div>
-							)
-						})}
-					</div>
-				)
-			})}
-		</div>
+		<>
+			<button type="button" onClick={manualMove}>
+				Manual move
+			</button>
+			<div className="board_container">
+				{mBoard.map((rowInfo, rowIndex) => {
+					return (
+						<div className="row" key={Math.random()}>
+							{rowInfo.map((x, squareIndex) => {
+								const isSnake = validateSquare(
+									snake,
+									Number(rowIndex),
+									Number(squareIndex)
+								)
+								return (
+									<div
+										className={`square ${isSnake ? 'is-snake' : ''}`}
+										key={Math.random()}
+									></div>
+								)
+							})}
+						</div>
+					)
+				})}
+			</div>
+		</>
 	)
 }
 
